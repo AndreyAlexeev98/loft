324ce69bdbc0330025784f2c644c408bd0fce3f0
Andrey Alexeev <ipalexeev98@mail.ru>
2021-03-27 09:14:52 +0300

    task in checked

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,45 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+function random(from, to) {
+  return parseInt(from + Math.random() * to - from);;
+}
 
-export function createDiv() {}
+let currentDrag;
+let startX = 0;
+let startY = 0;
+
+document.addEventListener('mousemove', (e) => {
+  if (currentDrag == true) {
+    currentDrag.style.left = e.clientX - startX + 'px';
+    currentDrag.style.top = e.clientY - startY + 'px';
+  } 
+});
+
+export function createDiv() {
+  let div = document.createElement('div');
+  let minNum = 20;
+  let maxNum = 200;
+  let maxColor = 0xffffff;
+
+  div.className = 'draggable-div';
+  div.style.background = "#" + random(0, maxColor).toString(16);
+  div.style.left = random(0, window.innerWidth) + 'px';
+  div.style.top = random(0, window.innerHeight) + 'px';
+  div.style.width = random(minNum, maxNum) + 'px';
+  div.style.height = random(minNum, maxNum) + 'px';
+
+  div.addEventListener ('mousedown', function (event) {
+    currentDrag = div;
+    startX = event.offsetX;
+    startY = event.offsetY;
+  });
+
+  div.addEventListener('mouseup', () => (currentDrag = false));
+
+  return div;
+}
+
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
